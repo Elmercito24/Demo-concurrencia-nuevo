9229388cc1a73b7777f4435aa52d01e5e3caee15
Elmer Vásquez Guevara <vasquezguevaraelmer75@gmail.com>
2024-11-21 13:17:54 -0500

    Agregar archivos de proyecto.

---
+++ DemoConcurrencia/DemoConcurrencia/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace DemoConcurrencia
+{
+    partial class FrmConcurrencia
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            splitContainer1 = new SplitContainer();
+            BtnCancelarHilo = new Button();
+            BtnIniciarTarea = new Button();
+            BtnIniciarHilo = new Button();
+            btnIniciarSecuencial = new Button();
+            TxtResultado = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.BackColor = Color.FromArgb(192, 255, 192);
+            splitContainer1.Panel1.Controls.Add(BtnCancelarHilo);
+            splitContainer1.Panel1.Controls.Add(BtnIniciarTarea);
+            splitContainer1.Panel1.Controls.Add(BtnIniciarHilo);
+            splitContainer1.Panel1.Controls.Add(btnIniciarSecuencial);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(TxtResultado);
+            splitContainer1.Size = new Size(889, 545);
+            splitContainer1.SplitterDistance = 295;
+            splitContainer1.TabIndex = 0;
+            // 
+            // BtnCancelarHilo
+            // 
+            BtnCancelarHilo.BackColor = Color.FromArgb(255, 192, 192);
+            BtnCancelarHilo.Location = new Point(60, 357);
+            BtnCancelarHilo.Name = "BtnCancelarHilo";
+            BtnCancelarHilo.Size = new Size(189, 35);
+            BtnCancelarHilo.TabIndex = 3;
+            BtnCancelarHilo.Text = "Cancelar  procesos";
+            BtnCancelarHilo.UseVisualStyleBackColor = false;
+            BtnCancelarHilo.Click += BtnCancelarHilo_Click;
+            // 
+            // BtnIniciarTarea
+            // 
+            BtnIniciarTarea.BackColor = Color.FromArgb(224, 224, 224);
+            BtnIniciarTarea.Location = new Point(60, 238);
+            BtnIniciarTarea.Name = "BtnIniciarTarea";
+            BtnIniciarTarea.Size = new Size(189, 54);
+            BtnIniciarTarea.TabIndex = 2;
+            BtnIniciarTarea.Text = "Iniciar tarea";
+            BtnIniciarTarea.UseVisualStyleBackColor = false;
+            BtnIniciarTarea.Click += BtnIniciarTarea_Click;
+            // 
+            // BtnIniciarHilo
+            // 
+            BtnIniciarHilo.BackColor = Color.FromArgb(224, 224, 224);
+            BtnIniciarHilo.Location = new Point(56, 136);
+            BtnIniciarHilo.Name = "BtnIniciarHilo";
+            BtnIniciarHilo.Size = new Size(193, 55);
+            BtnIniciarHilo.TabIndex = 1;
+            BtnIniciarHilo.Text = "Iniciar hilo";
+            BtnIniciarHilo.UseVisualStyleBackColor = false;
+            BtnIniciarHilo.Click += BtnIniciarHilo_Click;
+            // 
+            // btnIniciarSecuencial
+            // 
+            btnIniciarSecuencial.BackColor = Color.FromArgb(224, 224, 224);
+            btnIniciarSecuencial.Location = new Point(56, 38);
+            btnIniciarSecuencial.Name = "btnIniciarSecuencial";
+            btnIniciarSecuencial.Size = new Size(193, 51);
+            btnIniciarSecuencial.TabIndex = 0;
+            btnIniciarSecuencial.Text = "Iniciar Secuencial";
+            btnIniciarSecuencial.UseVisualStyleBackColor = false;
+            btnIniciarSecuencial.Click += btnIniciarSecuencial_Click;
+            // 
+            // TxtResultado
+            // 
+            TxtResultado.BackColor = Color.White;
+            TxtResultado.Dock = DockStyle.Fill;
+            TxtResultado.Location = new Point(0, 0);
+            TxtResultado.Multiline = true;
+            TxtResultado.Name = "TxtResultado";
+            TxtResultado.Size = new Size(590, 545);
+            TxtResultado.TabIndex = 0;
+            // 
+            // FrmConcurrencia
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(889, 545);
+            Controls.Add(splitContainer1);
+            Name = "FrmConcurrencia";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Demostracion de concurrencia";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private SplitContainer splitContainer1;
+        private Button BtnIniciarTarea;
+        private Button BtnIniciarHilo;
+        private Button btnIniciarSecuencial;
+        private TextBox TxtResultado;
+        private Button BtnCancelarHilo;
+    }
+}
---
+++ DemoConcurrencia/DemoConcurrencia/Form1.cs
@@ -0,0 +1,99 @@
+using System.Linq.Expressions;
+using System.Net;
+
+namespace DemoConcurrencia
+{
+    public partial class FrmConcurrencia : Form
+    {
+        //Vamos a definir un metood privado para cancelar los metodos
+        //Fuente de cancelacion de tokens
+        private CancellationTokenSource _cts;
+
+        public FrmConcurrencia()
+        {
+            InitializeComponent();
+        }
+        private void btnIniciarSecuencial_Click(object sender, EventArgs e)
+        {
+
+            ActualizarResultado("Iniciando proceso secuencial...");
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(1000);//Que se demore 1 segundo
+                ActualizarResultado($"Actividad - paso {i + 1}");
+            }
+            ActualizarResultado("Fin del proceso secuencial");
+        }
+        private void ActualizarResultado(string mensaje)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action(() => ActualizarResultado(mensaje)));
+                return;
+            }
+            TxtResultado.AppendText($"{DateTime.Now:HH:mm:ss.fff}:{mensaje}{Environment.NewLine}");//Environment.line sirve para que salga otra linea
+        }
+
+        //BOTON INICIAR HILO
+        private void BtnIniciarHilo_Click(object sender, EventArgs e)
+        {
+            _cts = new CancellationTokenSource();
+            var token = _cts.Token;//Creamos la variable token
+            Thread hilo1 = new Thread(() =>
+            {
+                try//Se pulsa dos veces tab para que se cree automaticamnete try y catch
+                {
+                    ActualizarResultado($"Iniciar nuevo hilo {Thread.CurrentThread.ManagedThreadId}.....");
+                    Thread.Sleep(1000);//que se demore 1 segundo en ejecutar.
+                    for (int i = 0; i < 5; i++)
+                    {
+                        token.ThrowIfCancellationRequested();//Dedemos agregar este codigo para que pueda cancelar
+                        Thread.Sleep(1000);//Que se demore 1 segundo
+                        ActualizarResultado($"Actividad en hilo: {Thread.CurrentThread.ManagedThreadId}- Paso{i + 1}");
+                    }
+
+                    ActualizarResultado($"Hilo {Thread.CurrentThread.ManagedThreadId}terminado");
+
+                }
+                catch (OperationCanceledException)
+                {
+                    ActualizarResultado("Hilo cancelado");
+                }         
+            });
+            hilo1.Start();//Para que se inicialize el hilo
+        }
+
+        //BOTON INICIAR TAREA
+        //Para cada vez que utilizamos await tenemos que utlizar async.
+        private async void BtnIniciarTarea_Click(object sender, EventArgs e)
+        {
+            _cts = new CancellationTokenSource();
+            var token = _cts.Token;//Creamos la variable token.
+            await Task.Run(() =>
+            {
+                try
+                {
+                    //Task es tarea ("Iniciando task") o ("Iniciar tarea") es igual.
+                    ActualizarResultado($"Iniciando tarea{Task.CurrentId}.....");
+                    for (int i = 0; i < 5; i++)
+                    {
+                        token.ThrowIfCancellationRequested();//Dedemos agregar este codigo para que pueda cancelar
+                        Thread.Sleep(1000);//Que se tarde 1 segundo en ejecutar.
+                        ActualizarResultado($"Actividad en tarea {Task.CurrentId} -paso{i + 1}");
+                    }
+                    ActualizarResultado($"Tarea {Task.CurrentId}completada");
+                }
+                catch (Exception)
+                {
+
+                    ActualizarResultado("Tarea cancelada");
+                }
+            });
+        }
+        private void BtnCancelarHilo_Click(object sender, EventArgs e)
+        {
+           _cts?.Cancel();//El signo de interrogacion sirve para no cancele cuando no se encuentre nada que pueda cancelar.
+        }
+    }
+}
+    
